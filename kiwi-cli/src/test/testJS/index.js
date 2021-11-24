cc102875338872e326ba752046f3241119479e1d
EP-zhaoxin <ep@EP-zhaoxindeMacBook-Pro.local>
2021-11-24 14:20:00 +0800

    优化测试demo

---
+++ kiwi-cli/src/test/testJS/index.js
@@ -0,0 +1,9 @@
+function a() {
+  const carryBill = [];
+  const p = '用户名';
+  const p2 = '个人中心';
+  const p3 = '我的收藏';
+  const p4 = '插件';
+  const p5 = 'eee';
+  const p6 = `你只有${carryBill.length}条分提运单，无法再增加其他收货人信息`
+}
--- kiwi-cli/src/test/testJS/index.ts
+++
@@ -1,7 +0,0 @@
-function a() {
-  const p = '用户名';
-  const p2 = '个人中心';
-  const p3 = '我的收藏';
-  const p4 = '插件';
-  const p5 = 'eee'
-}
